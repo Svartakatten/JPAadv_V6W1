7eeb894a4db6ebd6da110e905e4b7ff318b155ed
Joakim Englund <joakimenglund@protonmail.com>
2025-12-15 15:08:32 +0100

    Created Customer

---
+++ src/main/java/se/chasacademy/databaser/jpaorders/Models/Customer.java
@@ -0,0 +1,80 @@
+package se.chasacademy.databaser.jpaorders.Models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CUST_ID")
+    private Long id;
+
+    @Column(name = "CUST_FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "CUST_LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL_ADDR")
+    private String email;
+
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    public Customer() {}
+
+    public Customer(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Customer(String firstName, String lastName, String email, LocalDateTime createdAt) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
